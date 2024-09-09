58c1b2b4344c2df2a41f11af74b4a9dd988122d2
alex cappelletti <cappelle@fbk.eu>
2024-09-09 09:36:36 +0000

    set reponame into vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/bird-classy-u/",
   plugins: [
     Vue({
       template: { transformAssetUrls }
